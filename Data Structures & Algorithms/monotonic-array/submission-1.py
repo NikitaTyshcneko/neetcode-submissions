05643bf7bbe172b1f07218a14e291f18e0b46994
Nikita Tyshchenko <nikitatyshchenkoofficial@gmail.com>
2026-09-07 13:27:00 +0200

    Add: monotonic-array - submission-1

---
+++ Data Structures & Algorithms/monotonic-array/submission-1.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        if nums[0] < nums[-1]: return nums == sorted(nums)
+        else: return nums == sorted(nums, reverse=True)
+        
